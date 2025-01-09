443763ecce1e21cce6c45a7ab1652cd218fbb710
Aiden149 <giftedfredrick16@gmail.com>
2025-01-09 23:14:39 +0300

    Update xvid.js

--- lazackcmds/xvid.js
+++ lazackcmds/xvid.js
@@ -29,7 +29,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
         m.chat,
         Buffer.from(buffer),
         `${title}.mp4`,
-        `𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 Here is your Xvideos video: ${title}`
+        `𝐀𝐈𝐃𝐄𝐍_𝐓𝐄𝐂𝐇 𝐁𝐎𝐓 Here is your Xvideos video: ${title}`
       )
     } else {
       // If it's not a valid URL, perform a search and display the search results
